1b855e0e5d614236ae33e410399545511010cb3b
Zeus <purslh1@unlv.nevada.edu>
2024-05-29 14:09:57 -0700

    0.0.9 added 404 page view

---
+++ views/error404.jsx
@@ -0,0 +1,15 @@
+// Require react and Def
+const React = require('react')
+const Def = require('./default')
+
+// Create error 404 stub func and export
+function error404 () {
+    return(
+        <Def>
+            <h1>Error 404: PAGE NOT FOUND</h1>
+            <p>The requested page cannot be found.</p>
+        </Def>
+    )
+}
+
+module.exports = error404
