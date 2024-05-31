02ff4bf802aab736bd28fea61c09bfa62d8e6d50
Zeus <purslh1@unlv.nevada.edu>
2024-05-31 14:20:12 -0700

    0.1.5 added bootstrapping grid to index page

--- views/error404.jsx
+++ views/error404.jsx
@@ -8,6 +8,12 @@ function error404 () {
         <Def>
             <main>
                 <h1>Error 404: PAGE NOT FOUND</h1>
+                <div>
+                    <img src="https://placehold.co/250" alt="Placeholder" />
+                    <div>
+                        Photo by <a href="AUTHOR_LINK">Author Name</a> on <a href="SITE_LINK">Placehold</a>
+                    </div>
+                </div>
                 <p>The requested page cannot be found.</p>
             </main>
         </Def>
