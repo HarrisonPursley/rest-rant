d1c9395b5d065c57dfe2dccf8839f90692fa8bd4
Zeus <purslh1@unlv.nevada.edu>
2024-06-03 14:21:11 -0700

    0.2.4 added edit functionality

--- controllers/places.js
+++ controllers/places.js
@@ -64,6 +64,28 @@ router.get('/:id/edit', (req, res) => {
       res.render('places/edit', { place: places[id] })
     }
   })
-  
+
+router.put('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    }
+    else if (!places[id]) {
+        res.render('error404')
+    }
+    else {
+        if (!req.body.pic) {
+            req.body.pic = 'https://placehold.co/250'
+        }
+        if (!req.body.city) {
+            req.body.city = 'Anytown'
+        }
+        if (!req.body.state) {
+            req.body.state = 'USA'
+        }
+        places[id] = req.body
+        res.redirect(`/places/${id}`)
+    }
+})
 
 module.exports = app
