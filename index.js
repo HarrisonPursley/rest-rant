907757ce58431bea32e0eea71929ffc9142c5a02
Zeus <purslh1@unlv.nevada.edu>
2024-06-03 13:59:25 -0700

    0.2.2 delete functionality implemented

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // require and initialize express app
 const express = require('express')
+const methodOverride = require('method-override')
 const app = express()
 
 // require dotenv
@@ -11,6 +12,7 @@ app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 // import router from places.js
 app.use('/places', require('./controllers/places'))
